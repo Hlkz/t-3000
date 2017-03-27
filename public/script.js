76b9d1394921d0b25728906a6fd2700558fcaf0a
Hlkz <hlkz@hotmail.fr>
2017-03-27 19:09:37 +0200

    init

---
+++ public/script.js
@@ -0,0 +1,158 @@
+var socket = io('/', { query: 'r_var='+window.location.pathname })
+
+// conf
+let ALLOW_CMD_DELAY = 500 // ms
+// Default values, equal to server default values
+let MAX_NUMBER = 2
+let WAITING_TIME = 20000
+let WAITING_TIME2 = 10000
+let ENABLE_OVERWRITE = false
+// misc
+let stateTimeout = null
+let ALLOW_CMD = true
+
+let GetDiv = id => document.getElementById(id)
+let ShowDiv = (id, html=null) => {
+  let el = GetDiv(id)
+  el.style.display = 'block'
+  if (html) el.innerHTML = html
+}
+let HideDiv = id => { GetDiv(id).style.display = 'none' }
+let IsShown = id => GetDiv(id).style.display === 'block'
+let renderMain = h => { ShowDiv('main', h) }
+let renderSecond = h => { ShowDiv('second', h) }
+let hideSecond = () => { HideDiv('second') }
+let isSecondShown = () => IsShown('second')
+let renderCount = h => { ShowDiv('count', h) }
+let hideCount = () => { HideDiv('count') }
+
+let keyEventListener =  event => {
+  var key = event.keyCode
+  if (key == 9 || key == 27)
+    event.preventDefault()
+  if (document.hasFocus() && document.activeElement.tagName == 'INPUT') {
+    if (key == 27)
+      document.activeElement.trigger('blur')
+    return
+  }
+  // View change
+  if (key == 9) { // Tab
+    TogglePanel()
+    return
+  }
+  // Send info to server
+  if (!ALLOW_CMD) return
+  if (key == 13 || key == 32) // Enter Space
+    SendState()
+  else if (key > 47 && key < Math.max(58, 49+MAX_NUMBER)) // 123456789
+    SendState(key-48)
+  else if (key > 96 && key < 100) // NumPad 123
+    SendMode(key-96)
+  else if (key == 107)
+    SendWait()
+  else return
+  ALLOW_CMD = false
+  setTimeout(()=>{ ALLOW_CMD = true }, ALLOW_CMD_DELAY)
+}
+let clickListener = event => {
+  if (!ALLOW_CMD) return
+  SendState()
+  ALLOW_CMD = false
+  setTimeout(()=>{ ALLOW_CMD = true }, ALLOW_CMD_DELAY)
+}
+document.addEventListener('keydown', keyEventListener, true)
+document.getElementById('topright').addEventListener('click', clickListener, false)
+
+function Beep() { new Audio('/jingle.mp3').play() }
+
+function TogglePanel() {
+  if (IsShown('panel'))
+    HideDiv('panel')
+  else
+    ShowDiv('panel')
+}
+
+function ToggleEnableOverwrite() {
+  SendEnableOverwrite(!ENABLE_OVERWRITE)
+}
+
+function Wait(time) {
+  renderCount('<div class="txt">'+Math.floor(time/1000)+'</div>')
+  if (time > 999) {
+    clearTimeout(stateTimeout)
+    stateTimeout = setTimeout(()=>{ Wait(time-1000) }, 1000)
+  }
+  else
+    hideCount()
+}
+
+function SetMode(mode) {
+  MAX_NUMBER = mode.maxNumber
+  document.getElementById('input_maxNumber').value = MAX_NUMBER
+  document.documentElement.style.background = mode.background
+  document.documentElement.style.color = mode.color
+}
+
+function SetConf(conf) {
+  MAX_NUMBER = conf.maxNumber
+  WAITING_TIME = conf.waitingTime
+  WAITING_TIME2 = conf.waitingTime2
+  ENABLE_OVERWRITE = conf.enableOverwrite
+  document.getElementById('input_maxNumber').value = MAX_NUMBER
+  document.getElementById('input_waitingTime').value = WAITING_TIME
+  document.getElementById('input_waitingTime2').value = WAITING_TIME2
+  document.getElementById('button_enableOverwrite').innerHTML = ENABLE_OVERWRITE ? 'Overwrite: on' : 'Overwrite: off'
+}
+
+function SendState(state) {
+  socket.emit('state', { state, instant: state==null?0:1 })
+}
+function SendWait() {
+  socket.emit('wait')
+}
+function SendMode(mode) {
+  socket.emit('mode', { mode })
+}
+function SendMaxNumber() {
+  socket.emit('maxNumber', { maxNumber: document.getElementById('input_maxNumber').value })
+}
+function SendWaitingTime() {
+  socket.emit('waitingTime', { waitingTime: document.getElementById('input_waitingTime').value })
+}
+function SendWaitingTime2() {
+  socket.emit('waitingTime2', { waitingTime2: document.getElementById('input_waitingTime2').value })
+}
+function SendEnableOverwrite(enableOverwrite) {
+  socket.emit('enableOverwrite', { enableOverwrite })
+}
+
+socket.on('state', function(data) {
+  renderMain(data.html)
+  if (data.state > 0)
+    clearTimeout(stateTimeout)
+  if (data.release && !isSecondShown() || data.state > 0)
+    Beep()
+  if ((data.release || data.state > 0) && isSecondShown()) {
+    hideSecond()
+    hideCount()
+  }
+})
+
+socket.on('init', data => {
+  renderMain(data.html)
+  SetMode(data.mode)
+  SetConf(data.conf)
+})
+
+socket.on('mode', data => {
+  SetMode(data.mode)
+})
+
+socket.on('conf', data => {
+  SetConf(data.conf)
+})
+
+socket.on('confirm', data => {
+  renderSecond(data.html)
+  Wait(data.wait)
+})
---
+++ server.js
@@ -0,0 +1,147 @@
+var fs = require('fs')
+var path = require('path')
+var bodyParser = require('body-parser')
+
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+
+let config = require('./config')
+const SERVER_PORT = config.port
+
+let ROOMs = {}
+
+app.set('port', SERVER_PORT)
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Cache-Control', 'no-cache')
+  next()
+})
+
+app.get('/*', function(req,res) {
+  res.sendFile(__dirname+'/public/index.html');
+})
+
+function CreateRoom(room) {
+  ROOMs[room] = {
+    room,
+    state: 0,
+    stateTimeout: null,
+    mode: 2,
+    maxNumber: 2,
+    waitingTime: 20000,
+    waitingTime2: 10000,
+    enableOverwrite: false
+  }
+}
+
+const cartes = [ 'Zener_Cercle.jpg', 'Zener_Croix.jpg', 'Zener_Vague.jpg', 'Zener_Carre.jpg', 'Zener_Etoile.jpg' ]
+function GenHTML(Room, forceState=null) {
+  let state = forceState || Room.state
+  if (state < 1)
+    return '<div class="txt">TRANSITION</div>'
+  switch(Room.mode) {
+    default:
+    case 1: return '<div class="txt">'+state+'</div>' // Number
+    case 2: return '<div class="txt">'+(state>1?'Non':'Oui')+'</div>' // Yes/No
+    case 3: return '<img class="img" src="./symbol/'+cartes[state-1]+'">' // Zener
+  }
+}
+
+function SetMode(Room, mode, maxNumber = null) {
+  Room.mode = mode
+  if (mode === 1) Room.maxNumber = maxNumber || 10
+  else if (mode === 2) Room.maxNumber = 2
+  else if (mode === 3) Room.maxNumber = 5
+  // console.log('Mode change:', mode)
+  io.to(Room.room).emit('mode', { mode: GetMode(Room) })
+}
+
+function GetMode(Room) {
+  return {
+    maxNumber: Room.maxNumber,
+    background: Room.mode ===3 ? 'white' : 'black',
+    color: Room.mode === 3 ? 'black' : 'white'
+  }
+}
+
+function GetConf(Room) {
+  return {
+    maxNumber: Room.maxNumber,
+    waitingTime: Room.waitingTime,
+    waitingTime2: Room.waitingTime2,
+    enableOverwrite: Room.enableOverwrite
+  }
+}
+
+function SendConf(Room) {
+  io.to(Room.room).emit('conf', { conf: GetConf(Room) })
+}
+
+function setState(Room, state, release=false) {
+  Room.state = state
+  // console.log('State change:', state)
+  io.to(Room.room).emit('state', { state, html: GenHTML(Room), release })
+}
+function SetState(Room, state) {
+  setState(Room, 0)
+  clearTimeout(Room.stateTimeout)
+  Room.stateTimeout =  setTimeout(()=>{ setState(Room, state, true); Room.stateTimeout = null }, state ? Room.waitingTime : Room.waitingTime2)
+}
+
+io.on('connection', function (socket) {
+  let room = socket.handshake['query']['r_var'];
+  if (!ROOMs[room])
+    CreateRoom(room)
+  let Room = ROOMs[room]
+  socket.join(room);
+  socket.emit('init', { html: GenHTML(Room), mode: GetMode(Room), conf: GetConf(Room) })
+  socket.on('state', function (data) {
+    let state = data.state
+    let instant = data.instant
+    if (state == null)
+      state = Math.floor(Math.random()*Room.maxNumber)+1
+    if (state > -1 && state <= Room.maxNumber && (Room.enableOverwrite || !Room.stateTimeout)) {
+      console.log('Post received:', (instant?'':'not ')+'instant')
+      if (instant > 0)
+        setState(Room, state)
+      else {
+        socket.emit('confirm', { html: GenHTML(Room, state), wait: Room.waitingTime })
+        SetState(Room, state)
+      }
+    }
+  })
+  socket.on('wait', function (data) {
+    socket.emit('confirm', { html: GenHTML(Room), wait: Room.waitingTime2 })
+    SetState(Room, 0)
+  })
+  socket.on('mode', function (data) {
+    if (data.mode > 0 && data.mode < 4)
+      SetMode(Room, data.mode)
+  })
+  socket.on('maxNumber', function (data) {
+    Room.maxNumber = data.maxNumber
+    SendConf(Room)
+  })
+  socket.on('waitingTime', function (data) {
+    Room.waitingTime = data.waitingTime
+    SendConf(Room)
+  })
+  socket.on('waitingTime2', function (data) {
+    Room.waitingTime2 = data.waitingTime2
+    SendConf(Room)
+  })
+  socket.on('enableOverwrite', function (data) {
+    Room.enableOverwrite = data.enableOverwrite
+    SendConf(Room)
+  })
+})
+
+server.listen(SERVER_PORT, function() {
+  console.log('Server up: http://localhost:' + SERVER_PORT + '/')
+})
